baa3143f24b1847e469837ff27cf9e74a266372a
Arjit Pratap Singh Chauhan <singhyashpratap2003@gmail.com>
2025-05-06 10:20:21 +0530

    Initial Commit

---
+++ AHA Smart Homes/AHA_Smart_HomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AHA_Smart_HomesApp.swift
+//  AHA Smart Homes
+//
+//  Created by Arjit Pratap Singh Chauhan on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AHA_Smart_HomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AHA Smart Homes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AHA Smart Homes
+//
+//  Created by Arjit Pratap Singh Chauhan on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
